0cbabd24a87ac6793dce8bbfa1eb4e4b65413adb
levit-14 <agarcialev23@gmail.com>
2026-03-23 19:27:58 -0700

    Nuevo models

--- blog/models.py
+++ blog/models.py
@@ -1,44 +1,36 @@
 from django.db import models
 
-
-# Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255)
 
-
     class Meta:
-        ordering = ('title')
+        ordering = ('title',)
         verbose_name_plural = 'Categories'
-
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-
-
     ACTIVE = 'active'
     DRAFT = 'draft'
 
-
-    CHOICES_STATUS = {
-        (ACTIVE, 'active')
-        (DRAFT, 'draft')
-    }
-
+    CHOICES_STATUS = [
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft'),
+    ]
 
     category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=225)
     intro = models.TextField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.DateTimeField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
     image = models.ImageField(upload_to='upload/', blank=True, null=True)
-
 
     def __str__(self):
         return self.title
+
    
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -46,7 +38,6 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.name
