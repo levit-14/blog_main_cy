cdaf9f9f16788dbb9d528ccd0c93fb5f0113fa64
levit-14 <agarcialev23@gmail.com>
2026-04-16 19:13:06 -0700

    Cambio

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,17 @@
-
 from django.shortcuts import render
 
 
 # Create your views here.
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
-
-
+from django.shortcuts import render, get_object_or_404, redirect
 from.models import Post, Category
+from .forms import CommentForm
 
 
 # Create your views here.
 def home(request):
+
+
     posts = Post.objects.filter(status=Post.ACTIVATE).order_by('-created_at')
     context = {
 
@@ -25,7 +25,21 @@ def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVATE)
 
 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+           
+            return redirect('post_detail', id=id)
+    else:
+        form = CommentForm()
+       
     context = {
         'post': post,
+        'form': form
     }
     return render(request, 'blog/detail.html', context)
